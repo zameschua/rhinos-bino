471a964250329c79be8ce9170ec983ba2eb2ef13
zames <zames.chua@wafflepos.com>
2022-11-10 19:21:13 +0800

    Fix up calculation

--- src/App.tsx
+++ src/App.tsx
@@ -54,114 +54,122 @@ const App = () => {
           maxWidth={400}
           width={"100%"}
           backgroundColor={"white"}
-          padding={4}
           borderRadius={"16"}
         >
-          <Text fontSize={24} fontWeight={"medium"} marginBottom={4}>
-            Rhino ball-sighting calculator
-          </Text>
-
-          <Box marginBottom={8}>
-            <Text fontSize={20} fontWeight={"semibold"} marginBottom={2}>
-              Number of shots
+          <Box padding={4}>
+            <Text fontSize={24} fontWeight={"semibold"}>
+              L2 Zeroing Calculator
             </Text>
-            <Divider marginBottom={2} />
-            <NumberInput
-              min={3}
-              max={5}
-              defaultValue={numberOfShots}
-              onChange={(valueAsString, valueAsNumber) => {
-                if (isNaN(valueAsNumber)) {
-                  return;
-                }
-
-                setNumberOfShots(valueAsNumber);
-
-                let newReadings: {
-                  azimuth: string;
-                  elevation: string;
-                }[] = _.cloneDeep(readings);
-                if (valueAsNumber > readings.length) {
-                  while (newReadings.length < valueAsNumber) {
-                    newReadings.push({
-                      azimuth: "",
-                      elevation: "",
-                    });
-                  }
-                } else {
-                  newReadings = _.slice(newReadings, 0, valueAsNumber);
-                }
-                setReadings(newReadings);
-              }}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
+            <Text fontSize={12} color={"gray.500"}>
+              By HQ1711
+            </Text>
           </Box>
 
-          {readings.map((reading, index) => {
-            return (
-              <Box key={index}>
-                <Text fontSize={20} fontWeight={"semibold"} marginBottom={2}>
-                  Shot {index + 1}
-                </Text>
-                <Divider marginBottom={2} />
-                <Box marginBottom={8}>
-                  <Box marginBottom={4}>
-                    <Text fontWeight={"medium"} color={"gray.600"}>
-                      Azimuth {index + 1}
-                    </Text>
-                    <Input
-                      type={"number"}
-                      value={reading.azimuth}
-                      onChange={(event) => {
-                        setReadings(
-                          readings.map((readingInner, indexInner) => {
-                            if (indexInner !== index) {
-                              return readingInner;
-                            }
+          <Divider />
 
-                            return {
-                              azimuth: event.target.value,
-                              elevation: reading.elevation,
-                            };
-                          })
-                        );
-                      }}
-                      placeholder={"0.00"}
-                    />
-                  </Box>
-                  <Box>
-                    <Text fontWeight={"medium"} color={"gray.600"}>
-                      Elevation {index + 1}
-                    </Text>
-                    <Input
-                      type={"number"}
-                      value={reading.elevation}
-                      onChange={(event) => {
-                        setReadings(
-                          readings.map((readingInner, indexInner) => {
-                            if (indexInner !== index) {
-                              return readingInner;
-                            }
+          <Box padding={4}>
+            <Box marginBottom={8}>
+              <Text fontSize={20} fontWeight={"semibold"} marginBottom={2}>
+                Number of shots
+              </Text>
+              <Divider marginBottom={2} />
+              <NumberInput
+                min={3}
+                max={5}
+                defaultValue={numberOfShots}
+                onChange={(valueAsString, valueAsNumber) => {
+                  if (isNaN(valueAsNumber)) {
+                    return;
+                  }
 
-                            return {
-                              azimuth: reading.azimuth,
-                              elevation: event.target.value,
-                            };
-                          })
-                        );
-                      }}
-                      placeholder={"0.00"}
-                    />
+                  setNumberOfShots(valueAsNumber);
+
+                  let newReadings: {
+                    azimuth: string;
+                    elevation: string;
+                  }[] = _.cloneDeep(readings);
+                  if (valueAsNumber > readings.length) {
+                    while (newReadings.length < valueAsNumber) {
+                      newReadings.push({
+                        azimuth: "",
+                        elevation: "",
+                      });
+                    }
+                  } else {
+                    newReadings = _.slice(newReadings, 0, valueAsNumber);
+                  }
+                  setReadings(newReadings);
+                }}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+
+            {readings.map((reading, index) => {
+              return (
+                <Box key={index}>
+                  <Text fontSize={20} fontWeight={"semibold"} marginBottom={2}>
+                    Shot {index + 1}
+                  </Text>
+                  <Divider marginBottom={2} />
+                  <Box marginBottom={8}>
+                    <Box marginBottom={4}>
+                      <Text fontWeight={"medium"} color={"gray.600"}>
+                        Azimuth {index + 1}
+                      </Text>
+                      <Input
+                        type={"number"}
+                        value={reading.azimuth}
+                        onChange={(event) => {
+                          setReadings(
+                            readings.map((readingInner, indexInner) => {
+                              if (indexInner !== index) {
+                                return readingInner;
+                              }
+
+                              return {
+                                azimuth: event.target.value,
+                                elevation: reading.elevation,
+                              };
+                            })
+                          );
+                        }}
+                        placeholder={"0.000"}
+                      />
+                    </Box>
+                    <Box>
+                      <Text fontWeight={"medium"} color={"gray.600"}>
+                        Elevation {index + 1}
+                      </Text>
+                      <Input
+                        type={"number"}
+                        value={reading.elevation}
+                        onChange={(event) => {
+                          setReadings(
+                            readings.map((readingInner, indexInner) => {
+                              if (indexInner !== index) {
+                                return readingInner;
+                              }
+
+                              return {
+                                azimuth: reading.azimuth,
+                                elevation: event.target.value,
+                              };
+                            })
+                          );
+                        }}
+                        placeholder={"0.000"}
+                      />
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
-            );
-          })}
+              );
+            })}
+          </Box>
         </Box>
         <Center
           position={"fixed"}
@@ -260,18 +268,16 @@ const ResultModal = ({
 }) => {
   const azimuth: number = useMemo(() => {
     return (
+      -2 *
       (_.sumBy(readings, (reading) => parseFloat(reading?.azimuth ?? 0)) /
-        readings.length) *
-        2 -
-      1
+        readings.length)
     );
   }, [readings]);
   const elevation: number = useMemo(() => {
     return (
+      -2 *
       (_.sumBy(readings, (reading) => parseFloat(reading?.elevation ?? 0)) /
-        readings.length) *
-        2 -
-      1
+        readings.length)
     );
   }, [readings]);
   return (
